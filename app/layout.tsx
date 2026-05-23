7133d1ddeda5c0503f1099718a49cce91ac4aec2
bijaya143 <nemomajhi14@gmail.com>
2026-05-23 15:17:48 +0545

    chore: added tracking code from clarity and vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import "@/styles/globals.css";
 import AppContent from "@/components/AppContent";
 import ToasterProvider from "@/providers/ToasterProvider";
 import { SITE_NAME } from "@/lib/constant";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -65,6 +68,17 @@ export default function RootLayout({
       >
         <AppContent>{children}</AppContent>
         <ToasterProvider />
+        <Analytics />
+        <SpeedInsights />
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "wvjame99u7");
+          `}
+        </Script>
       </body>
     </html>
   );
